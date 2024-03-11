45d44c60c355c99a17fad5df37dab91bb992fac2
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2024-03-11 23:07:21 +0100

    Add UserResult component to fetch users from github API

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,29 @@
 function About() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the
+        <a href='https://www.udemy.com/course/modern-react-front-to-back'>
+          {' '}
+          React Front To Back
+        </a>{' '}
+        Udemy course by
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-grey-400'>
+        Layout By:
+        <a href='https://twitter.com/hassibmoddasser' className='text-white'>
+          {' '}
+          Hassib Moddasser
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default About;
