8c72baf7d4573b6dfa710caefea16f939fd605f5
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2024-03-24 22:06:08 +0100

    Building alert context and reducer

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from 'react';
+import alertReducer from './AlertReducer';
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  // Set an alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: { msg, type },
+    });
+  };
+
+  return (
+    <AlertContext.Provider value={{ alert: state }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
